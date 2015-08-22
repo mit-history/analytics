d7175c74b0dfd58ba9c00b5a0f36085a26fa0275
Christopher W York <christopher.w.york@gmail.com>
2015-08-22 17:35:43 +0100

    Register using svg widget works as well as anything

--- js/register.js
+++ js/register.js
@@ -12,84 +12,92 @@ require('../css/register.css')
 var hg = require('mercury')
 var h = require('mercury').h
 
-var extend = require('xtend');
+var svg = require('virtual-hyperscript/svg');
 
 var d3 = require('d3')
 
-const format = d3.time.format("%Y-%m-%d");
+const format = d3.time.format('%Y-%m-%d');
+
+/*
+* SVG based image viewer, with zoom and pan transforms.
+*
+* Pipes events through d3's zoom behavior for cross-browser consistency.
+*
+* I decided to break with unidirectional flow of data and store image scale
+* and translation in a small widget because
+*
+* (1) this gives better user experience during zoom (no jerkiness)
+* (2) from the application's point of view, zoom state is transient
+* (3) scale & translate persist even when user chooses a new register page
+* (4) feeding d3's zoom event back through mercury's dom-delegator is doable,
+*     but complex; dom-delegator is really intended for DOM events
+*/
+
+function ImageWidget(url) {
+  if (!(this instanceof ImageWidget)) {
+    return new ImageWidget(url)
+  }
+  this.url = url
+}
+
+ImageWidget.prototype.type = 'Widget'
+
+ImageWidget.prototype.init = function() {
+  var start_scale = 0.33
+  var xmlns = 'http://www.w3.org/2000/svg';
+  var elem = document.createElementNS(xmlns, 'svg')
+
+  var svg = d3.select(elem)
+      .attr('width', '100%')
+      .attr('height', '100%')
+
+  var image = svg.append('image')
+      .attr('width', 1655)
+      .attr('height', 2255)
+      .attr('transform', 'scale(' + start_scale + ')')
+
+  var overlay = svg.append('rect')
+    .attr('width', '100%')
+    .attr('height', '100%')
+    .attr('fill', 'none')
+
+  svg.call(d3.behavior.zoom()
+      .scaleExtent([0.15, 10.0])
+      .scale(start_scale)
+      .on('zoom', () => {
+        image.attr('transform', 'translate(' + d3.event.translate + ') ' + 'scale(' + d3.event.scale + ')')
+      }))
+
+  return elem
+}
+
+ImageWidget.prototype.update = function(prev, elem) {
+  d3.select(elem)
+    .select('image')
+      .attr('xlink:href', this.url)
+}
 
 // Register component proper
 
 function Register() {
   return hg.state({
     url: hg.value(null),
-    scale: hg.value(1.0),
-    translate: hg.value([0,0]),
     channels: {
-      setDate: Register.setDate,
-      setScale: setScale,
-      setTranslate: setTranslate
+      setDate: Register.setDate
     }
   })
 }
 
 Register.setDate = function(state, url, date) {
-  d3.text(url + "/image?date=" + date, (err, data) => {
+  d3.text(url + '/image?date=' + date, (err, data) => {
     if (err) throw err
     state.url.set(data)
   })
 }
 
-function setScale(state, new_scale) {
-  state.scale.set(new_scale)
-}
-
-function setTranslate(state, new_translate) {
-  state.translate.set(new_translate)
-}
-
-// Pipe DOM-side event handling through D3 to a custom event
-
-function ZoomHook(scale, translate) {
-  this.scale = scale
-  this.translate = translate
-}
-
-// pity to have to use a global... but mercury proxies the event, stripping all info
-var zoom_stats = {}
-
-ZoomHook.prototype.hook = function (elem, prop) {
-  elem.setAttribute(prop, 'transform: scale(' + this.scale + '); transform-origin: 0px 0px')
-  var zoom = d3.behavior.zoom()
-    .scaleExtent([0, 1.0])
-    .scale(this.scale)
-    .translate(this.translate)
-    .on('zoom', () => {
-      // create a custom event wrapper to dispatch to the DOM element
-      zoom_stats = { scale: zoom.scale(), translate: zoom.translate() }
-      elem.dispatchEvent(new Event('zoom'))
-    })
-  d3.select(elem).call(zoom)
-}
-
-hg.Delegator().listenTo('zoom')
-
-
-// Rendering for Register
-
 Register.render = function(state) {
-
-  var scaleEvent = hg.BaseEvent(handleScale)
-  function handleScale(ev, broadcast) {
-    broadcast(zoom_stats.scale)
-  }
-
-  return (
-    h('img', { src: state.url,
-               style: new ZoomHook(state.scale, state.translate),
-               'ev-zoom': scaleEvent(state.channels.setScale)
-             }, [])
-  )
+//  return ImageWidget(state.url)
+  return ImageWidget(state.url)
 }
 
 
