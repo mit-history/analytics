ea1e052c0f9e65b6ccb989745f74641dc82563b6
Christopher W York <christopher.w.york@gmail.com>
2015-08-22 19:19:09 +0100

    Register using D3 zoom + DOM img tag
    After much experimentation with canvas, svg, mercury handlers, etc:
    
    Pipes events through d3's zoom behavior for cross-browser consistency.
    
    I decided to break with flex+react style unidirectional flow of data
    and store image scale & translation in a small widget because:
    
    (1) this gives better user experience during zoom (no jerkiness)
    (2) from the application's point of view, zoom state is transient
    (3) BUT scale & translate persist even when user chooses a new register page
    (4) AND feeding d3's zoom event back through mercury's dom-delegator is doable,
            but complex; dom-delegator is really intended for DOM events

--- js/register.js
+++ js/register.js
@@ -19,62 +19,83 @@ var d3 = require('d3')
 const format = d3.time.format('%Y-%m-%d');
 
 /*
-* SVG based image viewer, with zoom and pan transforms.
+* DOM based image viewer, with zoom and pan transforms vis CSS3
 *
 * Pipes events through d3's zoom behavior for cross-browser consistency.
 *
-* I decided to break with unidirectional flow of data and store image scale
-* and translation in a small widget because
+* I decided to break with flex+react style unidirectional flow of data
+* and store image scale & translation in a small widget because:
 *
 * (1) this gives better user experience during zoom (no jerkiness)
 * (2) from the application's point of view, zoom state is transient
-* (3) scale & translate persist even when user chooses a new register page
-* (4) feeding d3's zoom event back through mercury's dom-delegator is doable,
-*     but complex; dom-delegator is really intended for DOM events
+* (3) BUT scale & translate persist even when user chooses a new register page
+* (4) AND feeding d3's zoom event back through mercury's dom-delegator is doable,
+*         but complex; dom-delegator is really intended for DOM events
 */
 
-function ImageWidget(url) {
-  if (!(this instanceof ImageWidget)) {
-    return new ImageWidget(url)
-  }
+function ImageWidget(url, start_scale) {
   this.url = url
+  this.start_scale = start_scale || 1.0
 }
 
 ImageWidget.prototype.type = 'Widget'
 
 ImageWidget.prototype.init = function() {
-  var start_scale = 0.33
-  var xmlns = 'http://www.w3.org/2000/svg';
-  var elem = document.createElementNS(xmlns, 'svg')
+  // create container and image
+  var register = document.createElement('div')
+  register.className = 'register'
 
-  var svg = d3.select(elem)
-      .attr('width', '100%')
-      .attr('height', '100%')
+  var img = document.createElement('img')
+  img.style['transform-origin'] = '0 0'
+  register.appendChild(img)
 
-  var image = svg.append('image')
-      .attr('width', 1655)
-      .attr('height', 2255)
-      .attr('transform', 'scale(' + start_scale + ')')
+  // prepare d3 zoom behavior
+  var zoom = d3.behavior.zoom()
+    .scale(this.start_scale)
+    .on('zoom', zoomed)
 
-  var overlay = svg.append('rect')
-    .attr('width', '100%')
-    .attr('height', '100%')
-    .attr('fill', 'none')
+  d3.select(register)
+    .call(zoom)
 
-  svg.call(d3.behavior.zoom()
-      .scaleExtent([0.15, 10.0])
-      .scale(start_scale)
-      .on('zoom', () => {
-        image.attr('transform', 'translate(' + d3.event.translate + ') ' + 'scale(' + d3.event.scale + ')')
-      }))
+  rescale([0,0], this.start_scale)
 
-  return elem
+  return register
+
+  function zoomed() {
+    // only now are register dimensions set correctly
+    var parentWidth = register.offsetWidth
+    var parentHeight = register.offsetHeight
+    var min_scale = Math.min(parentWidth / img.width, parentHeight / img.height)
+
+    // get current position
+    var s = Math.max(d3.event.scale, min_scale)
+    var [tx, ty] = d3.event.translate
+
+    // no dragging beyond bounds
+    tx = Math.max(Math.min(tx, 0), parentWidth - img.width * s)
+    ty = Math.max(Math.min(ty, 0), parentHeight - img.height * s)
+
+    // center when possible
+    tx = Math.min(tx, Math.max(0, (parentWidth - img.width * s) / 2.0))
+    ty = Math.min(ty, Math.max(0, (parentHeight - img.height * s) / 2.0))
+
+    // update zoom behavior and dom element
+    zoom.scaleExtent([min_scale, 1.2])
+      .scale(s)
+      .translate([tx, ty])
+
+    rescale([tx, ty], s)
+  }
+
+  function rescale(translate, scale) {
+    img.style.transform = 'translate(' + translate.map( (r) => r + 'px').join(', ') + ') ' +
+                          'scale(' + scale + ')'
+  }
 }
 
-ImageWidget.prototype.update = function(prev, elem) {
-  d3.select(elem)
-    .select('image')
-      .attr('xlink:href', this.url)
+ImageWidget.prototype.update = function(prev, register) {
+  var img = register.querySelector('img')
+  img.src = this.url
 }
 
 // Register component proper
@@ -96,8 +117,7 @@ Register.setDate = function(state, url, date) {
 }
 
 Register.render = function(state) {
-//  return ImageWidget(state.url)
-  return ImageWidget(state.url)
+  return new ImageWidget(state.url, 0.4)
 }
 
 
