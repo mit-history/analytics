5ffdb5bcd947decb57298328d096134270963e50
Christopher W York <christopher.w.york@gmail.com>
2016-08-14 19:00:01 +0100

    Set webpack defaults for staging

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ module.exports = {
   entry: './index.js',
   output: {
     path: __dirname + '/build/',
-    publicPath: '/build/',
+    publicPath: '/analytics/build/',	// Staging
+    // publicPath: '/js/app/', 				// Production
     filename: 'bundle.js'
   },
   module: {
@@ -20,4 +21,3 @@ module.exports = {
     new webpack.NoErrorsPlugin()
   ]
 };
-
