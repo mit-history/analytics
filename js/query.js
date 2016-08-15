9d3de1e6b9b9f5f008567eb6317d6ef721621844
Guliavan <herr.talbot@gmail.com>
2016-08-14 20:47:21 -0400

    Started to implement feature

--- js/query.js
+++ js/query.js
@@ -259,6 +259,11 @@ Query.render = function (app_state, modal_state, query_state, lang) {
                     ]),
 
                     h('header.query-pane-section', [
+                        h('h2', msgs[lang]['comparison_tool_place_title']),
+                        TimePeriod.render(app_state, modal_state, query_state, lang),
+                    ]),
+
+                    h('header.query-pane-section', [
                         h('h2', msgs[lang]['comparison_tool_time_scope_title']),
                         TimePeriod.render(app_state, modal_state, query_state, lang),
                     ]),
